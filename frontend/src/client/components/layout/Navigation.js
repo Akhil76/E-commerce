b8c64c286f96ffebdc7b1d1184cfb09c6fcac3f1
akhil79 <akhilpaulweb@gmail.com>
2022-01-31 11:37:16 +0600

    Adding new responsive navigation.

--- frontend/src/client/components/layout/Navigation.js
+++ frontend/src/client/components/layout/Navigation.js
@@ -6,6 +6,12 @@ import {fetch_catagory} from '../../../statemanager/actions/catagory';
 import {NavLink} from 'react-router-dom';
 
 
+const customStyles = {
+    my_nav:{
+
+    },
+
+}
 
 class Navigation extends React.Component{
     componentDidMount(){
@@ -15,42 +21,37 @@ class Navigation extends React.Component{
     render(){
         const {catagories} = this.props;
         return(
-                <AppBar position="sticky">  
-                   <div className="my_nav"> 
-                    <ul>
-                    {
-                     catagories.map((item)=>
-                       
-                        <li><NavLink to={"/catagory/"+item._id}>{item.CatagoryName}</NavLink>
-                            <div className="sub_menu">
-                                
-                                 {
-                                     item.Subcatagory.map((sub_item)=>
-                                     
-                                    <ul>
-                                        <li className="hover_me"><NavLink to={"/subcatagory/"+sub_item._id}>{sub_item.SubcatagoryName}</NavLink>
-                                            <div className="sub_menu2">
-                                                {
-                                                    sub_item.Subcatagory_two.map((sub_item_two)=>
-                                                    <ul>
-                                                        <li><NavLink to={"/product/"+sub_item_two._id}>{sub_item_two.Subcatagory_twoName}</NavLink></li>
-                                                    </ul>  
-                                                    )
-                                                }
-                                                
-                                            </div>
-                                        </li>
-                                       
-                                    </ul>
-                                )}
-                               
-                            </div>
-                        </li>
-                     )
-                    }  
-                    </ul>
-                   </div>    
-                </AppBar> 
+            <AppBar className="navbar" position="sticky">
+            <div className="nav">
+                <ul>
+                {catagories.map((item)=>
+                    <li>
+                        <NavLink to={"/catagory/"+item._id}>{item.CatagoryName}</NavLink>{item.Subcatagory.length!==0 && <span>+</span>}
+                        <div class="dropdown-1">
+					        <ul>
+                                {
+                                item.Subcatagory.map((sub_item)=>
+                                <li>
+                                    <NavLink to={"/subcatagory/"+sub_item._id}>{sub_item.SubcatagoryName}</NavLink>{sub_item.Subcatagory_two.length!==0 && <span>+</span>}
+                                    <div className="dropdown-2">
+                                        <ul>
+                                            {
+                                            sub_item.Subcatagory_two.map((sub_item_two)=>
+                                            <ul>
+                                                <li><NavLink to={"/product/"+sub_item_two._id}>{sub_item_two.Subcatagory_twoName}</NavLink></li>
+                                            </ul>  
+                                            )}
+                                        </ul>
+                                    </div>
+                                </li>)
+                                }
+                            </ul>
+                        </div>    
+                    </li>
+                )}
+                </ul>
+            </div>
+          </AppBar>
         )
     }
 }
