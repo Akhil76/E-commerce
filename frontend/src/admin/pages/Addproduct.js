af915c1a6f8de378a4d4eacbf59ce588cbb710e9
Akhil Paul <84143623+Akhil76@users.noreply.github.com>
2022-02-25 23:23:05 +0600

    addproduct class component is turned into func com

--- frontend/src/admin/pages/Addproduct.js
+++ frontend/src/admin/pages/Addproduct.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React,{ useState,useEffect } from "react";
 import { Button, Paper, TextField, Typography } from "@material-ui/core";
-import { connect } from "react-redux";
+import {useDispatch,useSelector} from "react-redux";
 import { mainCatagory, subcatagory, subcatagorytwo } from "../../statemanager/actions/catagorySelect";
 import { addproduct } from "../../statemanager/actions/product";
 
 
-class Addproduct extends React.Component {
+function Addproduct(){
+    const dispatch = useDispatch();
+    const catagories = useSelector((state)=>state.catagorySelect);
+    const subcatagories = useSelector((state)=>state.subcatagorySelect);
+    const subcattwo = useSelector((state)=>state.subcattwoSelect);
 
-    state = {
+    
+    const [ productData,setProductData] = useState({
         ProductName: "",
         catagoryId: "",
         subcatagoryId: "",
@@ -17,54 +22,61 @@ class Addproduct extends React.Component {
         Description: "",
         ProductImg: "",
         imgpreview: "",
-    }
+    });
+    useEffect(()=>{
+        dispatch(mainCatagory());
+        dispatch(subcatagory(productData.catagoryId));
+        dispatch(subcatagorytwo(productData.subcatagoryId));
+    },[productData.catagoryId,productData.subcatagoryId]);
 
-    changeHandler = event => {
-        this.setState({
-            [event.target.name]: event.target.value
-        })
-    };
+    
 
-    onChangeFile = event => {
-        // console.log(event.target.files[0]);
+    // changeHandler = event => {
+    //     this.setState({
+    //         [event.target.name]: event.target.value
+    //     })
+    // };
 
-        this.setState({ ProductImg: event.target.files[0] });
+    // onChangeFile = event => {
+    //     // console.log(event.target.files[0]);
 
-        // for image preview------------------
-        const file = event.target.files[0];
+    //     this.setState({ ProductImg: event.target.files[0] });
 
-        const reader = new FileReader();
-        reader.onloadend = () => {
-            this.setState({
-                file: file,
-                imgpreview: reader.result
-            });
-        };
-        if (file) {
-            reader.readAsDataURL(file);
-        }
+    //     // for image preview------------------
+    //     const file = event.target.files[0];
 
-        //--------------------------------------
-    }
+    //     const reader = new FileReader();
+    //     reader.onloadend = () => {
+    //         this.setState({
+    //             file: file,
+    //             imgpreview: reader.result
+    //         });
+    //     };
+    //     if (file) {
+    //         reader.readAsDataURL(file);
+    //     }
+
+    //     //--------------------------------------
+    // }
 
 
 
-    submitHandler = event => {
-        event.preventDefault()
+    const submitHandler =(e)=> {
+        e.preventDefault();
         const formdata = new FormData();
 
-        formdata.append("ProductName", this.state.ProductName);
-        formdata.append("catagoryId", this.state.catagoryId)
-        formdata.append("subcatagoryId", this.state.subcatagoryId);
-        formdata.append("subcatagorytwoId", this.state.subcatagorytwoId);
-        formdata.append("Quantity", this.state.Quantity);
-        formdata.append("Price", this.state.Price);
-        formdata.append("Description", this.state.Description);
-        formdata.append("ProductImg", this.state.ProductImg);
+        formdata.append("ProductName", productData.ProductName);
+        formdata.append("catagoryId", productData.catagoryId)
+        formdata.append("subcatagoryId", productData.subcatagoryId);
+        formdata.append("subcatagorytwoId", productData.subcatagorytwoId);
+        formdata.append("Quantity", productData.Quantity);
+        formdata.append("Price", productData.Price);
+        formdata.append("Description", productData.Description);
+        formdata.append("ProductImg",productData.ProductImg);
 
-        this.props.addproduct(formdata);
+        dispatch(addproduct(formdata));
 
-        this.setState({
+        setProductData({
             ProductName: "",
             catagoryId: "",
             subcatagoryId: "",
@@ -78,77 +90,55 @@ class Addproduct extends React.Component {
 
     }
 
-    componentDidMount() {
-        this.props.mainCatagory();
+    // componentDidMount() {
+    //     this.props.mainCatagory();
 
-    }
-
-
-
-    componentDidUpdate() {
-        if (this.state.catagoryId !== "") {
-            this.props.subcatagory(this.state.catagoryId);
-
-        };
-        if (this.state.subcatagoryId !== "") {
-            this.props.subcatagorytwo(this.state.subcatagoryId);
-        }
-
-    }
+    // }
 
 
-    render() {
 
-        const { catagories, subcatagories, subcattwo } = this.props;
+    // componentDidUpdate() {
+    //     if (this.state.catagoryId !== "") {
+    //         this.props.subcatagory(this.state.catagoryId);
 
-        const { ProductName,
-            catagoryId,
-            subcatagoryId,
-            subcatagorytwoId,
-            Quantity,
-            Price,
-            Description,
-            imgpreview,
-        } = this.state;
+    //     };
+    //     if (this.state.subcatagoryId !== "") {
+    //         this.props.subcatagorytwo(this.state.subcatagoryId);
+    //     }
+
+    // }
 
         return (
             <div>
-                <Paper style={{ padding: "20px" }}>
+                <Paper style={{ padding: "10px" }}>
                     <Typography
                         variant="h5"
                         style={{ background: "#82b1ff", padding: "10px" }}
                         align="center"
                     >Add Product</Typography>
-                    <p>{ProductName}</p>
-                    <form onSubmit={this.submitHandler} >
-                        <div style={{ display: "flex", justifyContent: "space-between" }}>
-                            <div style={{ width: "48%" }}>
-                                <Typography>Product Name : </Typography>
-                                <TextField
-                                    fullWidth
-                                    size="small"
-                                    variant="outlined"
-                                    className=""
-                                    type="text"
-                                    placeholder="Product Name"
-                                    Name="ProductName"
-                                    id="ProductName"
-                                    value={this.ProductName}
-                                    onChange={this.changeHandler}
-                                />
-                                <TextField
-                                    fullWidth
-                                    size="small"
-                                    variant="outlined"
-                                    className=""
-                                    type="text"
-                                    placeholder="Product Name"
-                                    Name="ProductName"
-                                    id="ProductName"
-                                    value={this.ProductName}
-                                    onChange={this.changeHandler}
-                                />
-                            </div>
+                    <div>
+                        <p>{productData.ProductName}</p>
+                        <p>{productData.Quantity}</p>
+                        <p>{productData.Price}</p>
+                        <p>{productData.Description}</p>
+                    </div>
+                    <form onSubmit={submitHandler}>
+                        <div style={{ marginTop: "10px" }}>
+                            <Typography>Product Name : </Typography>
+                            <TextField
+                                fullWidth
+                                size="small"
+                                variant="outlined"
+                                className=""
+                                type="text"
+                                placeholder="Product Name"
+                                Name="ProductName"
+                                id="ProductName"
+                                value={productData.ProductName}
+                                onChange={(e)=>setProductData({...productData,ProductName:e.target.value})}
+                            />
+                        </div>
+                        <div style={{ marginTop: "10px", display: "flex", justifyContent: "space-between" }}>
                             <div style={{ width: "48%" }}>
                                 <Typography>Quantity : </Typography>
                                 <TextField
@@ -160,70 +150,72 @@ class Addproduct extends React.Component {
                                     placeholder="0"
                                     Name="Quantity"
                                     id="Quantity"
-                                    value={this.Quantity}
-                                    onChange={this.changeHandler}
+                                    value={productData.Quantity}
+                                    onChange={(e)=>setProductData({...productData,Quantity:e.target.value})}
+                                />
+                            </div>
+                            <div style={{ width: "48%" }}>
+                                <Typography>Price : </Typography>
+                                <TextField
+                                    fullWidth
+                                    size="small"
+                                    variant="outlined"
+                                    className=""
+                                    type="number"
+                                    placeholder="0"
+                                    Name="Price"
+                                    id="Price"
+                                    value={productData.Price}
+                                    onChange={(e)=>setProductData({...productData,Price:e.target.value})}
                                 />
                             </div>
                         </div>
-
-                        <div>
-                            <Typography>Choose a catagory : </Typography>
-                            <select 
-                                style={{height:"30px"}}
-                                id="catagory"
-                                name="catagoryId"
-                                value={this.catagoryId}
-                                onChange={this.changeHandler}
-                            >
-                                <option value="">Select one</option>
-                                {catagories.map(item => <option value={item._id}>{item.CatagoryName}</option>)}
-                            </select>
-                        </div>
-                        {subcatagories.length !== 0 &&
+                        <div style={{marginTop:"10px"}}>
                             <div>
-                                <Typography>Choose a sub catagory : </Typography>
+                                <Typography>Choose a category : </Typography>
                                 <select
-                                style={{height:"30px"}} 
-                                id="subcatagory" 
-                                name="subcatagoryId" 
-                                value={this.subcatagoryId} 
-                                onChange={this.changeHandler}
+                                    style={{ height: "30px" }}
+                                    id="catagory"
+                                    name="catagoryId"
+                                    value={productData.catagoryId}
+                                    onChange={(e)=>setProductData({...productData,catagoryId:e.target.value})}
                                 >
                                     <option value="">Select one</option>
-                                    {subcatagories.map(item => <option value={item._id}>{item.SubcatagoryName}</option>)}
+                                    {catagories.map(item => <option value={item._id}>{item.CatagoryName}</option>)}
                                 </select>
-                            </div>}
-                        {subcattwo.length !== 0 &&
-                            <div>
-                                <Typography>Choose a child catagory : </Typography>
-                                <select 
-                                style={{height:"30px"}}
-                                id="subcatagorytwo" 
-                                name="subcatagorytwoId" 
-                                value={this.subcatagorytwoId} 
-                                onChange={this.changeHandler}
-                                >
-                                    <option value="">Select one</option>
-                                    {subcattwo.map(item => <option value={item._id}>{item.Subcatagory_twoName}</option>)}
-                                </select>
-                            </div>}
-
-                        <div>
-                            <Typography>Price : </Typography>
-                            <TextField
-                                fullWidth
-                                size="small"
-                                variant="outlined"
-                                className=""
-                                type="number"
-                                placeholder="0"
-                                Name="Price"
-                                id="Price"
-                                value={this.Price}
-                                onChange={this.changeHandler}
-                            />
+                            </div>
+                            {subcatagories.length !== 0 &&
+                                <div style={{marginTop:"10px"}}>
+                                    <Typography>Choose a sub category : </Typography>
+                                    <select
+                                        style={{ height: "30px" }}
+                                        id="subcatagory"
+                                        name="subcatagoryId"
+                                        value={productData.subcatagoryId}
+                                        onChange={(e)=>setProductData({...productData,subcatagoryId:e.target.value})}
+                                    >
+                                        <option value="">Select one</option>
+                                        {subcatagories.map(item => <option value={item._id}>{item.SubcatagoryName}</option>)}
+                                    </select>
+                                </div>
+                            }
+                            {subcattwo.length !== 0 &&
+                                <div style={{marginTop:"10px"}}>
+                                    <Typography>Choose a child category : </Typography>
+                                    <select
+                                        style={{ height: "30px" }}
+                                        id="subcatagorytwo"
+                                        name="subcatagorytwoId"
+                                        value={productData.subcatagorytwoId}
+                                        onChange={(e)=>setProductData({...productData,subcatagorytwoId:e.target.value})}
+                                    >
+                                        <option value="">Select one</option>
+                                        {subcattwo.map(item => <option value={item._id}>{item.Subcatagory_twoName}</option>)}
+                                    </select>
+                                </div>
+                            }
                         </div>
-                        <div>
+                        <div style={{marginTop:"10px"}}>
                             <Typography>Description : </Typography>
                             <textarea
                                 style={{ width: "100%", margin: "10px 0px", padding: "10px", borderRadius: "5px" }}
@@ -232,8 +224,8 @@ class Addproduct extends React.Component {
                                 placeholder="Write a detailed description of the product."
                                 Name="Description"
                                 id="Description"
-                                value={this.Description}
-                                onChange={this.changeHandler}
+                                value={productData.Description}
+                                onChange={(e)=>setProductData({...productData,Description:e.target.value})}
                             />
                         </div>
                         <div>
@@ -243,8 +235,9 @@ class Addproduct extends React.Component {
                                 className=""
                                 type="file"
                                 name="ProductImg"
-                                onChange={this.onChangeFile}
+                                onChange={(e) => setProductData({ ...productData,ProductImg:e.target.files[0] })}
                             />
+                            {/* <div className="imgPreview"><img src={imgpreview} alt="" /></div> */}
                         </div>
                         <Button
                             variant="contained"
@@ -253,20 +246,10 @@ class Addproduct extends React.Component {
                         >
                             Add product</Button>
                     </form>
-                    <div className="imgPreview"><img src={imgpreview} alt="" /></div>
                 </Paper>
-
             </div>
         )
-    }
+
 }
 
-
-const mapStateToProps = state => ({
-    catagories: state.catagorySelect,
-    subcatagories: state.subcatagorySelect,
-    subcattwo: state.subcattwoSelect,
-});
-
-
-export default connect(mapStateToProps, { addproduct, mainCatagory, subcatagory, subcatagorytwo })(Addproduct);
+export default Addproduct;
